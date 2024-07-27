dcb0b8b1d9b474e321a2b2fc6294821179001da3
Nima Nourinejad <nima.nourinejad@gmail.com>
2024-07-27 14:09:17 +0300

    practice

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -8,14 +8,17 @@ class ContentRating extends Component {
     this.state = {
         likes: 0,
         dislikes: 0,
+		total: 0,
         handleLike:() => {
           this.setState((prevState) => ({
-            likes: prevState.likes + 1
+            likes: prevState.likes + 1,
+			total: prevState.total + 1,
           }));
         },
         handleDislike:() => {
           this.setState((prevState) => ({
-            dislikes: prevState.dislikes + 1
+            dislikes: prevState.dislikes + 1,
+			total: prevState.total + 1,
           }));
         }
         };
@@ -35,6 +38,12 @@ class ContentRating extends Component {
                 Dislike ({this.state.dislikes})
             </button>
         </div>
+		<div className='total-review-1'>
+			Total Reviews_1: {this.state.total}
+		</div>
+		<div className='total-review-2'>
+			Total Reviews_2: {this.state.likes + this.state.dislikes}
+		</div>
     </div>
      </>
     );
